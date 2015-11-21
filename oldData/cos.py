fda5abb8260dbb829f870ced1f4385ef70c6c474
Omnei <sethm1093@gmail.com>
2015-11-21 15:05:52 -0800

    Added Old Data

---
+++ oldData/cos.py
@@ -0,0 +1,14 @@
+import numpy as np
+import math
+
+def main():
+  for ii in np.arange(-6.3, 6.3, .1):
+    print str(ii) + ", " + str(math.sin(ii))
+
+
+
+
+
+
+if __name__ == "__main__":
+  main()
